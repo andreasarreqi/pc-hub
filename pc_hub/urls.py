352a823c18f600f41cd84f3ec3fd942b05310f4f
Andre <sarreqiandrea@gmail.com>
2023-07-08 16:18:02 +0000

    Created products app

--- pc_hub/urls.py
+++ pc_hub/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
+    path('products/', include(products.urls)),
     path('checkout/', include('home.urls')),
     path('bag/', include('home.urls')),
     path('contact', include('home.urls')),
