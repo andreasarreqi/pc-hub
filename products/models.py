153409ec0815a03d82742f4226607f66aa304578
Andre <sarreqiandrea@gmail.com>
2023-07-09 15:33:52 +0000

    Created the products model on the products folder

--- products/models.py
+++ products/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Products(models.Model):
+    """
+    The Database model containing the products scheme
+    """
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    price = models.DecimalField(default=False, null=True, blank=True)
+    in_stock = models.BooleanField(default=True, null=True)
+    quantity = models.DecimalField(max_digits=3, decimal_places=0, default=0)
+
+    def __str__(self):
+        return str(self.title)
